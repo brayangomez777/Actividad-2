0caad26b8ad53b1a969c4d1fdf9e822229513ed4
Brayan Gomez <brayangomezortega1983@gmail.com>
2024-03-01 11:43:24 -0500

    primer commit

---
+++ app.py
@@ -0,0 +1,29 @@
+# Definir variables
+pais = "Colombia"
+poblacion = 51
+area = 1.14
+
+# Operaciones matemáticas
+promedio = poblacion / area
+porcentaje = poblacion * 100 / 780
+potencia = area ** 2
+raiz = poblacion ** 0.5
+
+# Estructuras condicionales para verificar el área
+if area > 1:
+    print("Es un país grande")
+else:
+    print("Es un país pequeño")
+
+# Bucle para imprimir letras del país
+for letra in pais:
+    print(letra)
+
+# Imprimir datos en consola
+print("País:", pais)
+print("Población:", poblacion)
+print("Área:", area)
+print("Promedio:", promedio)
+print("Porcentaje:", porcentaje)
+print("Potencia:", potencia)
+print("Raíz:", raiz)
